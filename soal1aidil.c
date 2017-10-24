037141d673b47a8f873f31ea4a4a032746f2cee3
chen1704 <32873349+chen1704@users.noreply.github.com>
2017-10-24 21:16:54 +0700

    Update soal1aidil.c

--- soal1aidil.c
+++ soal1aidil.c
@@ -5,6 +5,9 @@ const char* stok[7]={"MP4A1", "PM2-V1" , "SPR-3", "SS2-V5", "SPG1-V3", "MINE"};
 int jumlah[7];
 
 void lihat(){
+	for(int i=0;i<6;i++){
+		printf("%s :%d\n", stok[i], jumlah[i]);	
+	}
 }
 
 void beli(){
