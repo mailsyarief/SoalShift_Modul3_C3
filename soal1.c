5ca44af51ff9581d8b58785127a2bd6bbb559166
chen1704 <32873349+chen1704@users.noreply.github.com>
2017-10-24 18:11:04 +0700

    Coba print

--- soal1.c
+++ soal1.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
 
+int main(){
+	const char *menu[3];
+	a[0] = "lihat stock";
+  a[1] = "beli senjata"
+    printf("Menu : \n1. Lihat Stock\t 2. Beli Senjata\n");
+  string menu;
+  scanf("%s", &menu);
+  if(menu==menu[0]){
+    printf("liat stok\n"); 
+  }
+  else if (menu==menu[1]){
+    printf("beli hehe\n");
+  }
+}
