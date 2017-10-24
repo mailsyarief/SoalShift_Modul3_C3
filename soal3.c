ee8dbdf399fccb9cb21a7af9269ac5bfc5bcce2b
mailsyarief <ismail8syarief@gmail.com>
2017-10-25 05:21:25 +0700

    Update soal3.c

--- soal3.c
+++ soal3.c
@@ -11,32 +11,76 @@ int StautsKepiting = 100;
 int opsi;
 
 
-void lohan()
+void* lohan()
 {
-	int StatusLohan = 100;
 	while(1)
 	{
-		sleep();
+		sleep(10);
+		StatusLohan -= 15;
+		
 	}
 }
 
 
-void kepiting()
+void* kepiting()
 {
-	int StautsKepiting = 100;
+	while(1)
+	{
+		sleep(20);
+		StautsKepiting -= 10;
+	}
+
 }
 
-int opsi;
+void makan()
+{
+	int maam;
+	printf("Kasih makanan\n");	
+	printf("1. lohan, 2. kepiting\n");
+	scanf("%d", &maam);
+	if(maam==1)
+	{
+		StatusLohan += 10;
+	}
+	else if (maam==2)
+	{	
+		StautsKepiting += 10;
+	}
+}
+
 int main()
 {
-	printf("1. lohan, 2. kepiting\n");
-	scanf("%d", opsi);
+	pthread_t lohan_t;
+	pthread_t kepiting_t;
+
+	
+	printf("1. menu 2. status lohan, 3. Status kepiting\n");
+	scanf("%d", &opsi);
+	while(1)
+	{
 	if(opsi==1)
 	{
-		lohan();
+		makan();
+
 	}
 	else if(opsi==2)
 	{
-		kepiting();
+		printf("Status lohan = %d\n", StatusLohan);
+		continue;
+			
 	}
+	else if(opsi ==3)
+	{
+		printf("Status kepiting = %d\n", StautsKepiting);		
+		continue;
+	}
+	else
+	{
+		break;
+	}
+	}
+
+	pthread_create(&(lohan_t),NULL,&StatusLohan,NULL);
+	pthread_create(&(kepiting_t),NULL,&StautsKepiting,NULL);
+	
 }
