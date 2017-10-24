09f934dab4c83b88404ae70bb3e4751b91bd6734
chen1704 <32873349+chen1704@users.noreply.github.com>
2017-10-25 06:27:40 +0700

    masih salah, dia langsung exit

--- soal3.c
+++ soal3.c
@@ -6,100 +6,69 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-int StatusLohan = 100;
-int StautsKepiting = 100;
+int Lohan=99;
+int Kepiting=99;
 
-
-void* lohan()
-{
-	while(1)
-	{
-		sleep(10);
-		StatusLohan -= 15;
-		if(StatusLohan>100 || StautsKepiting>100 || StatusLohan<=0 || StautsKepiting <=0)
-	{
-		printf("Game Berakhir");
-		exit(EXIT_FAILURE);
-	}
-		
-	}
-}
-
-
-void* kepiting()
-{
-	while(1)
-	{
+void *inikepiting(void *arg){
+	while(1){
 		sleep(20);
-		StautsKepiting -= 10;
-		if(StatusLohan>100 || StautsKepiting>100 || StatusLohan<=0 || StautsKepiting <=0)
-	{
-		printf("Game Berakhir");
-		exit(EXIT_FAILURE);
-	}
-	}
-
-}
-
-void makan()
-{
-	int maam;
-	printf("Kasih makanan\n");	
-	printf("1. lohan, 2. kepiting\n");
-	scanf("%d", &maam);
-	if(maam==1)
-	{
-		StatusLohan += 10;
-		if(StatusLohan>100 || StautsKepiting>100 || StatusLohan<=0 || StautsKepiting <=0)
-	{
-		printf("Game Berakhir");
-		exit(EXIT_FAILURE);
-	}
-	}
-	else if (maam==2)
-	{	
-		StautsKepiting += 10;
-		if(StatusLohan>100 || StautsKepiting>100 || StatusLohan<=0 || StautsKepiting <=0)
-	{
-		printf("Game Berakhir");
-		exit(EXIT_FAILURE);
-	}
+		Kepiting-=10;
+		if(Kepiting <= 0 || Kepiting > 100){
+			printf("Game berkahir\n");
+			exit(EXIT_FAILURE);		
+		}
 	}
 }
 
+void *inilohan(void *arg){
+	while(1){
+		sleep(10);
+		if(Lohan <= 10 || Lohan > 100){
+			printf("Game berakhir\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+void makan(){
+	printf("Anda akan memberi makan hewan.\n");
+	printf("Hewan yang ingin diberi makan\n");
+	printf("1. Lohan\t2.Kepiting\n");
+	int maam; scanf("%d", &maam);
+	if(maam==1){
+		printf("Lohan diberi makan\n");
+	}
+	if(maam==2){
+		printf("Kepiting diberi makan\n");
+	}
+}
 
 int main()
 {
 	pthread_t lohan_t, kepiting_t;
-	pthread_create(&lohan_t,NULL,StatusLohan,NULL);
-	pthread_create(&kepiting_t,NULL,StautsKepiting,NULL);	
+	pthread_create(&lohan_t,NULL,&inilohan,NULL);
+	pthread_create(&kepiting_t,NULL,&inikepiting,NULL);	
 
 	int opsi;
 
 	while(1)
 	{
-	printf("Status lohan = %d\n", StatusLohan);
-	printf("Status kepiting = %d\n", StautsKepiting);			
-	printf("1. berimakan 2. status\n");
-	scanf("%d", &opsi);
+		printf("Status lohan = %d\n", Lohan);
+		printf("Status kepiting = %d\n", Kepiting);			
+		printf("1. berimakan 2. status\n");
+		scanf("%d", &opsi);
 
-	if(opsi==1)
-	{
-		makan();
-
-	}
-	else if(opsi==2){
-		printf("Status Lohan: %d\n", StatusLohan);
-		printf("Status Kepiting: %d\n", StatusKepiting)
-	}
-	
-	if(StatusLohan>100 || StautsKepiting>100 || StatusLohan<=0 || StautsKepiting <=0)
-	{
-		printf("Game Berakhir");
-		exit(EXIT_FAILURE);
-	}
+		if(opsi==1){
+			makan();
+		}
+		else if(opsi==2){
+			printf("Status Lohan: %d\n", Lohan);
+			printf("Status Kepiting: %d\n", Kepiting);
+		}	
+		if(Lohan > 100 || Lohan <= 100 || Kepiting > 100 || Kepiting <= 100){
+			printf("Game berakhir\n");			
+			break;
+		}
 
 	}
-
-	
 }
