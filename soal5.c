00a11d33f3a0f2c8d7da1df78003bb29a8532fb6
chen1704 <32873349+chen1704@users.noreply.github.com>
2017-10-25 07:58:00 +0700

    Update soal5.c

--- soal5.c
+++ soal5.c
@@ -4,28 +4,36 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <pthread.h>
 
-FILE *target;
-
-void *Search()
+void *Search(void *arg)
 {
-	target = fopen("", r);
-
-
-
-
+	FILE *f;
+	f=fopen("Novel.txt", "r");
+	char kata[100];
+	strcpy(kata,arg);
+	int count;
+	while(getline(&line, &len, fileIn)!= -1){
+		if(strstr(line, kata)!=NULL){
+		count++;
+		}
+	}
+	printf("%s: %d\n",kata, count);
 }
 
 
 
-int main(int argc, char** argv[])
+int main(int argc, char* argv[])
 {
 
 	pthread_t t[argc];
 	int i;
-	for(1=0;i<argc;i++)
+	for(i=0;i<argc;i++)
 	{
 		pthread_create(&(t[i], NULL, &Search, (void*)argv[i]));
+	}
+	for(i=0;i<argc;i++){
+		pthread_join(tid[i], NULL);	
 	}
 
 	return 0;
