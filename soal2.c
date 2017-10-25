b100dd8f7e86a28ef861f10cd1c593af2d3ef2c5
chen1704 <32873349+chen1704@users.noreply.github.com>
2017-10-25 11:39:44 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,1 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
 
+pthread_t tid1;
+pthread_t tid2;
+
+int status, skorA, skorB;
+const char* a[2];
+int boardA[20];
+int boardB[20];
+
+void *playerA(void *arg){
+	
+}
+
+void *playerB(void *arg){
+
+}
+
+int main(){
+	for(int i=1;i<=16;i++){
+		boardA[i]=0;
+		boardB[i]=0;
+	}
+	
+	pthread_create(&(tid1), NULL , &playerA, NULL);
+	pthread create(&(tid2), NULL, &playerB, NULL);
+
+	pthread_join(tid1,NULL);
+	pthread_join(tid2, NULL);
+}
